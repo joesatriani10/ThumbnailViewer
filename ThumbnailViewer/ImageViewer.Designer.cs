94408b3e66939c59048ebf99d5634b71abbc61e8
areal <areal@calibratedhealthcare.com>
2022-03-10 14:41:54 -0800

    Add project files.

---
+++ ThumbnailViewer/ImageViewer.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ThumbnailViewer
+{
+    partial class ImageViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(776, 426);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mouseClickPreview);
+            // 
+            // ImageViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Name = "ImageViewer";
+            this.Text = "Image Viewer";
+            this.Load += new System.EventHandler(this.ImageViewerLoad);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ListView listView;
+    }
+}
---
+++ ThumbnailViewer/ImageViewer.cs
@@ -0,0 +1,66 @@
+﻿namespace ThumbnailViewer
+{
+    public partial class ImageViewer : Form
+    {
+        public ImageViewer(string folderPath)
+        {
+            InitializeComponent();
+            this.folderPath = folderPath;
+        }
+
+        private string folderPath = "";
+
+        private void ImageViewerLoad(object sender, EventArgs e)
+        {            
+            loadImages();
+        }
+
+        private void loadImages()
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(folderPath);
+
+            FileInfo[] files = dInfo.GetFiles("*.tif");// Or any other images in folder
+           
+            var imageList = new ImageList();
+            imageList.ImageSize = new Size(240, 320);
+
+            foreach (FileInfo file in files)
+            {
+                imageList.Images.Add(file.Name, Image.FromFile(file.FullName));
+            }
+
+            // tell your ListView to use the new image list
+            listView.LargeImageList = imageList;
+            var n = 0;
+            foreach (FileInfo file in files)
+            {
+                listView.Items.Add(file.Name, n++);
+            }
+
+        }
+
+        private void mouseClickPreview(object sender, MouseEventArgs e)
+        {            
+            String selected = Path.Join(folderPath, listView.SelectedItems[0].Text);
+
+            Form formView = new Form();
+
+            formView.FormBorderStyle = FormBorderStyle.FixedSingle;
+            formView.MaximizeBox = false;
+            formView.MinimizeBox = false;
+            formView.Width = 576;
+            formView.Height = 768;
+
+            var pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Image = Image.FromFile(selected);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            formView.Controls.Add(pictureBox);
+            formView.ShowDialog();
+            
+            pictureBox.Dispose();
+            formView.Dispose();            
+        }        
+    }
+}
